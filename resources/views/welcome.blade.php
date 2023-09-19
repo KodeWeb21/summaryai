f6b69d3479873959f63684f180066713752127ba
Bryant Mejia <bryantmejia21@gmail.com>
2023-09-19 17:46:02 -0400

    agregando meta etiquetas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="title" content="Transform long texts into concise summaries instantly.">
+    <meta name="description" content="Use our advanced AI to generate accurate and fast summaries of any text, optimize your time with our online text summary generator!">
+    <meta name="keywords" content="abstract generator, AI, artificial intelligence, automatic summaries, summary tool, online abstracts">
+    <meta name="author" content="SummaryAI">
+    <meta name="robots" content="index, follow">
+    <meta property="og:title" content="Transform long texts into concise summaries instantly.">
+    <meta property="og:description" content="Use our advanced AI to generate accurate and fast summaries of any text, optimize your time with our online text summary generator!">
+    <meta property="og:image" content="https://i.imgur.com/cFvLNhX.png">
+    <meta property="og:url" content="https://summaryai.dev">
     <link rel="shortcut icon" href="images/favicon.svg" type="image/x-icon">
     <style> 
         @font-face {     
