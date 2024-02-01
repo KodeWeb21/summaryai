c0439d529f51599b8225b11e35b733bcded29d9a
Bryant Mejia <bryantmejia21@gmail.com>
2024-02-01 01:08:58 -0400

    Creando nuevas secciones

---
+++ resources/views/frequent.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Frequent questions</title>
+    <meta name="title" content="solve all your questions about SummaryAI">
+    <meta name="description" content="how the site works, how monetization is handled, among other actions.">
+    <meta name="keywords" content="question, frequent, Frequent questions">
+    <meta name="author" content="SummaryAI">
+    <meta name="robots" content="index, follow">
+    <meta property="og:image" content="https://i.imgur.com/cFvLNhX.png">
+    <meta property="og:url" content="https://summaryai.dev">
+    <link rel="shortcut icon" href="images/favicon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/section.css') }}">
+</head>
+<body>
+<header class="header">
+        <span class="logo">
+        <a class="nav__link" href="/">SummaryAI</a>
+        </span>
+        <nav class="nav nav--hidden">
+          <ul class="nav__container">
+             <li class="nav__item"><a class="nav__link" href="/frequent">Frequent questions</a></li>
+             <li class="nav__item"><a class="nav__link" href="/privacy">Privacy</a></li>
+          </ul>
+        </nav>
+    </header>
+    <main class="container">
+        <h1>Frequent questions</h1>
+        <section class="quest">
+            <article>
+                <h2>Which is a summary.</h2>
+                <p class="text">A summary is a concise and abbreviated representation of a text, speech, event or any other form of extensive information. The main purpose of a summary is to condense the essential information, highlighting the key points and omitting less relevant details.</p>
+            </article>
+        </section>
+        <section class="quest">
+            <article>
+                <h2>How it works.</h2>
+                <p class="text">SummaryAI allows you to give users the ability to summarize using the power of artificial intelligence (AI).</p>
+                <p class="text">To accomplish our goal we use different APIs with trained artificial intelligence models.</p>
+            </article>
+        </section>
+        <section class="quest">
+            <article>
+                <h2>monetization.</h2>
+                <p class="text">We currently do not publish ads or charge for the use of the site. However, we will not deny that in the future this site may contain advertising.</p>
+            </article>
+        </section>
+    </main>
+    <footer class="footer">
+        <nav class="nav">
+          <ul class="nav__container">
+            <li class="nav__item"><a class="nav__link" href="/frequent">Frequent questions</a></li>
+            <li class="nav__item"><a class="nav__link" href="/privacy">Privacy</a></li>
+          </ul>
+        </nav>
+        </footer>
+</body>
+</html>
---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Privacy</title>
+    <meta name="title" content="solve all your questions about SummaryAI">
+    <meta name="description" content="how the site works, how monetization is handled, among other actions.">
+    <meta name="keywords" content="question, frequent, Frequent questions">
+    <meta name="author" content="SummaryAI">
+    <meta name="robots" content="index, follow">
+    <meta property="og:image" content="https://i.imgur.com/cFvLNhX.png">
+    <meta property="og:url" content="https://summaryai.dev">
+    <link rel="shortcut icon" href="images/favicon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/section.css') }}">
+</head>
+<body>
+    <header class="header">
+        <span class="logo">
+        <a class="nav__link" href="/">SummaryAI</a>
+        </span>
+        <nav class="nav nav--hidden">
+          <ul class="nav__container">
+             <li class="nav__item"><a class="nav__link" href="/frequent">Frequent questions</a></li>
+             <li class="nav__item"><a class="nav__link" href="/privacy">Privacy</a></li>
+          </ul>
+        </nav>
+    </header>
+    <main class="container">
+        <h1>Privacy</h1>
+        <section>
+        <p class="text">At SummaryAI we value and respect your privacy. This Privacy Policy explains how we collect, use and protect your personal information when you visit our website</p>
+        <section>
+            <article>
+                <h2>Information Collected</h2>
+                <p class="text">When you visit our site we may collect the following information: </p>
+                <ul>
+                    <li class="text">Contact information, such as name and email address, when you register or subscribe to our services.</li>
+                    <li class="text"> Technical information, including IP addresses, browser type, operating system and session details.</li>
+                    <li class="text"> Usage data, such as the pages you visit and the actions you take on our website.</li>
+                    <li class="text">Other information you voluntarily provide to us through forms, surveys or communication with us.</li>
+                </ul>  
+            </article>
+        </section>
+        <section>
+            <h3>Consent and Withdrawal of Consent</h3>
+            <p class="text">By using SummaryAI, you agree to the terms of this Privacy Policy.</p>
+        </section>
+        <section>
+            <article>
+                <h3>Changes to the Privacy Policy.</h3>
+                <p class="text">We reserve the right to update this Privacy Policy at any time. Any changes will be posted on this page with a new effective date.</p>
+            </article>
+        </section>
+        </section>
+    </main>
+    <footer class="footer">
+        <nav class="nav">
+          <ul class="nav__container">
+            <li class="nav__item"><a class="nav__link" href="/frequent">Frequent questions</a></li>
+            <li class="nav__item"><a class="nav__link" href="/privacy">Privacy</a></li>
+          </ul>
+        </nav>
+    </footer>
+</body>
+</html>
